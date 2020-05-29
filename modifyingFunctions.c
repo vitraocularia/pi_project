36f5e706ac62d5c7fbeb0df5541023eae07b1fcd
vitraocularia <pysiokot@MacBook-Pro-Bartosz-2.local>
2020-05-29 17:09:50 +0200

    decreasing and increasing the volume of the sound

--- modifyingFunctions.c
+++ modifyingFunctions.c
@@ -56,3 +56,53 @@ size_t partition(float table[], size_t left, size_t right)
     swap(&table[i], &table[right]);
     return i;
 }
+
+void volDecrease(float table[], size_t tableSize)
+{
+    int sectionSize = tableSize/100;
+    size_t index;
+
+    float multiplier = 1;
+
+    for (size_t i = 0; i < 100; i++)
+    {
+        index = i * sectionSize;
+
+        for (size_t j = 0; j < sectionSize; j++)
+        {
+            table[ index + j] *= multiplier;
+        }
+
+        multiplier -= 0.01;
+    }
+
+    for (size_t i = index + sectionSize; i < tableSize; i++)
+    {
+        table[i] *= 0;
+    }
+}
+
+void volIncrease(float table[], size_t tableSize)
+{
+    int sectionSize = tableSize/100;
+    size_t index;
+
+    float multiplier = 0;
+
+    for (size_t i = 0; i < 100; i++)
+    {
+        index = i * sectionSize;
+
+        for (size_t j = 0; j < sectionSize; j++)
+        {
+            table[ index + j] *= multiplier;
+        }
+
+        multiplier += 0.01;
+    }
+
+    for (size_t i = index + sectionSize; i < tableSize; i++)
+    {
+        table[i] *= 1;
+    }
+}
--- modifyingFunctions.h
+++ modifyingFunctions.h
@@ -4,3 +4,6 @@ void reverseTableRef(float [], size_t);
 void reverseTable(float [], float [], size_t);
 
 void quicksort(float [], size_t, size_t);
+
+void volDecrease(float [], size_t);
+void volIncrease(float [], size_t);
